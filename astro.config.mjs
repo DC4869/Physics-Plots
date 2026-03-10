e2c5f3b4c5afae0832ca1c92be54b080b95cd4cb
milescb <mcochranbranson@gmail.com>
2026-03-10 14:21:18 +0100

    update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   output: 'static',
   site: 'https://plots.milescb.com',
-  adapter: cloudflare(),
   image: {
     service: {
       entrypoint: 'astro/services/sharp',
